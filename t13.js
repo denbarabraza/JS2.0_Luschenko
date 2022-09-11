1626bed271179c9c14bb27fb3af288cbf655ca4c
denbarabraza <denis.bareischev@gmail.com>
2022-09-11 21:49:15 +0300

    update+

--- t13.js
+++ t13.js
@@ -1,14 +1,14 @@
 // Task1
 // СОздать массив
 
-let a = ["Den", "Bareischev", true, 1];
+let a = ["Den", "Bareisch34ev", true, 1];
 console.log(a);
 
 
 // Task2
 // СОздать массив  и вывести его в div
 
-let b = [ "Den", "Bareischev", true, 1];
+let b = [ "Den", "Bareischev23", true, 1];
 let out1 = document.querySelector('.out-1');
 out1.innerHTML = b;
 
@@ -21,7 +21,7 @@ console.log( c.length );
 // Task4
 // Создать массив  и вывести его 0ой, 3ий, 8ой элемент
 
-let d = [ "Den", "Bareischev", true, 1, "Hi!", 34, 15, "Privet"];
+let d = [ "Den", "Bar", true, 1, "Hi!", 34, 15, "Privet"];
 console.log( c[0] );
 console.log( c[3] );
 console.log( c[8] );
