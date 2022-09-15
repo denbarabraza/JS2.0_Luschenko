d93e08f37fc6c50e9ff608f9fd287b6f416d8ee4
denbarabraza <denis.bareischev@gmail.com>
2022-09-15 22:11:40 +0300

    update+

--- t17.js
+++ t17.js
@@ -24,13 +24,17 @@ document.querySelector('.b-2').onclick = ()=>{
 
 
 // Task3
-// При нажатии b-2 выполняете функцию f2. Функция должна добавить в набор s2 элементы, которые пользователь вводит в i-2. Функция должна выводить в консоль s2 после каждого добавления элемента.
+// При нажатии b-3 выполняете функцию f3. Функция должна удалить из набора s3 строку, которую пользователь вводит в i-3. Функция должна выводить в консоль s3 после каждого удаления элемента.
 
-let s2 = new Set();
+let s3 = new Set();
+s3.add('h');
+s3.add('b');
+s3.add('o');
+s3.add('h');
 
-let input = document.querySelector('.i-2');
+let input3 = document.querySelector('.i-3');
 
-document.querySelector('.b-2').onclick = ()=>{
-	s2.add(input.value);
-	console.log(s2);
+document.querySelector('.b-3').onclick = ()=>{
+	s3.delete(input3.value);
+	console.log(s3);
 }
