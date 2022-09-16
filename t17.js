258000b84f48872ca40eff2d4543fe0562d3fd7a
denbarabraza <denis.bareischev@gmail.com>
2022-09-16 21:13:33 +0300

    update+

--- t17.js
+++ t17.js
@@ -1,45 +1,45 @@
-// // Task1
-// // Создайте set s1. Добавьте в него три элемента 'h', 'b', 'o', 'h'. Добавление делайте через add. Выведите в консоль получившийся набор (set) s1.
+// Task1
+// Создайте set s1. Добавьте в него три элемента 'h', 'b', 'o', 'h'. Добавление делайте через add. Выведите в консоль получившийся набор (set) s1.
 
-// let s1 = new Set();
-// s1.add('h');
-// s1.add('b');
-// s1.add('o');
-// s1.add('h');
+let s1 = new Set();
+s1.add('h');
+s1.add('b');
+s1.add('o');
+s1.add('h');
 
-// console.log(s1);
-
-
-// // Task2
-// // При нажатии b-2 выполняете функцию f2. Функция должна добавить в набор s2 элементы, которые пользователь вводит в i-2. Функция должна выводить в консоль s2 после каждого добавления элемента.
-
-// let s2 = new Set();
-
-// let input = document.querySelector('.i-2');
-
-// document.querySelector('.b-2').onclick = ()=>{
-// 	s2.add(input.value);
-// 	console.log(s2);
-// }
+console.log(s1);
 
 
-// // Task3
-// // При нажатии b-3 выполняете функцию f3. Функция должна удалить из набора s3 строку, которую пользователь вводит в i-3. Функция должна выводить в консоль s3 после каждого удаления элемента.
+// Task2
+// При нажатии b-2 выполняете функцию f2. Функция должна добавить в набор s2 элементы, которые пользователь вводит в i-2. Функция должна выводить в консоль s2 после каждого добавления элемента.
 
-// let s3 = new Set();
-// s3.add('h');
-// s3.add('b');
-// s3.add('o');
-// s3.add('h');
+let s2 = new Set();
 
-// let input3 = document.querySelector('.i-3');
+let input = document.querySelector('.i-2');
 
-// document.querySelector('.b-3').onclick = ()=>{
-// 	s3.delete(input3.value);
-// 	console.log(s3);
-// }
+document.querySelector('.b-2').onclick = ()=>{
+	s2.add(input.value);
+	console.log(s2);
+}
 
-// Task 3
+
+// Task3
+// При нажатии b-3 выполняете функцию f3. Функция должна удалить из набора s3 строку, которую пользователь вводит в i-3. Функция должна выводить в консоль s3 после каждого удаления элемента.
+
+let s3 = new Set();
+s3.add('h');
+s3.add('b');
+s3.add('o');
+s3.add('h');
+
+let input3 = document.querySelector('.i-3');
+
+document.querySelector('.b-3').onclick = ()=>{
+	s3.delete(input3.value);
+	console.log(s3);
+}
+
+// Task 4
 // Перебор массивов for..in, for..of
 
 let tArr = {
