54fa1879c798569fe44b97fae346530701eaa991
denbarabraza <denis.bareischev@gmail.com>
2022-09-18 11:49:28 +0300

    update+

--- t18.js
+++ t18.js
@@ -14,7 +14,7 @@ console.log(arB1);
 
 let arA2 = [1, 2, 3, 4, 5, 13, 15, 16, 31, 16, 141, 27, 222];
 
-let arB2 = arA2.filter( item => item > 100 )
+let arB2 = arA2.filter(item => item > 100)
 
 console.log(arB2);
 
@@ -34,4 +34,18 @@ console.log(str31);
 
 let str32 = arA3.reverse();
 console.log(str32);
+
+
+// Task4
+// Создать строку и применить метод 'forEach'.
+
+let ar4 = [23, 24, 245, 5565];
+
+
+
+ar4.forEach(function(item, index){
+	console.log(index);
+	console.log(item);
+	console.log('--');
+})
 
