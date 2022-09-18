b27bd96c9fd23b211b83aa30e54c4c7c93c9364b
denbarabraza <denis.bareischev@gmail.com>
2022-09-18 11:37:15 +0300

    update+

---
+++ t18.js
@@ -0,0 +1,37 @@
+// Task1
+// Создать массив и применить метод 'map'.
+
+let arA1 = [1, 2, 3, 4, 5, 13, 15, 16, 31, 16, 141, 27];
+
+let arB1 = arA1.map(function (item, index) {
+	return item * 10;
+})
+
+console.log(arB1);
+
+// Task2
+// Создать массив и применить метод 'filter'.
+
+let arA2 = [1, 2, 3, 4, 5, 13, 15, 16, 31, 16, 141, 27, 222];
+
+let arB2 = arA2.filter( item => item > 100 )
+
+console.log(arB2);
+
+
+// Task3
+// Создать строку и применить метод 'join', 'split', 'reverse'.
+
+let str3 = 'hi, hello, ciao, привет'
+
+let arA3 = str3.split(',');
+console.log(arA3);
+arA3.push(' здарова');
+arA3.shift();
+
+let str31 = arA3.join();
+console.log(str31);
+
+let str32 = arA3.reverse();
+console.log(str32);
+
